882be7a488b893010c60e38279c7c81341ebfe6f
Arivan-Amin <arivanamin@gmail.com>
2025-02-17 21:08:17 +0300

    Add Micro Blog solution

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,51 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.IntStream;
-
 public class PangramChecker {
 
-    public static final int ASCII_CODE_OF_LOWERCASE_A = 97;
-    public static final int NUMBER_OF_LETTERS_IN_ALPHABET = 26;
-
-    Map<String, Boolean> lettersMap = new HashMap<>();
-
-    public PangramChecker () {
-        initializeAlphabetLettersMap();
-    }
-
-    public void initializeAlphabetLettersMap () {
-        for (int i = 0; i < NUMBER_OF_LETTERS_IN_ALPHABET; i++) {
-            lettersMap.put(String.valueOf((ASCII_CODE_OF_LOWERCASE_A + i)), false);
-        }
-    }
+    public static final int NUMBER_OF_ALPHABET_CHARACTERS = 26;
 
     public boolean isPangram (String input) {
-        String lowerCaseInput = input.toLowerCase();
-
-        IntStream.range(0, lowerCaseInput.length())
-            .mapToObj(i -> String.valueOf((lowerCaseInput.charAt(i))))
-            .filter(currentChar -> isLetterInAlphabet(currentChar) &&
-                isLetterNotEncounteredYet(currentChar))
-            .forEachOrdered(this::recordLetterPresence);
-
-        return areAllLettersPresent();
-    }
-
-    private boolean isLetterInAlphabet (String currentChar) {
-        return lettersMap.containsKey(currentChar);
-    }
-
-    private boolean isLetterNotEncounteredYet (String currentChar) {
-        return !lettersMap.get(currentChar);
-    }
-
-    private void recordLetterPresence (String currentChar) {
-        lettersMap.put(currentChar, true);
-    }
-
-    private boolean areAllLettersPresent () {
-        return lettersMap.values()
-            .stream()
-            .allMatch(aBoolean -> aBoolean);
+        return input.toLowerCase()
+            .chars()
+            .filter(Character::isLetter)
+            .distinct()
+            .count() == NUMBER_OF_ALPHABET_CHARACTERS;
     }
 }
